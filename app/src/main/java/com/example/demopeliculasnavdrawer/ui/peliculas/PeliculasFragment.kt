9248fa18c1c5cc0c2ac84b1f91999bd6141a0b78
javjuarez <sonico@ciencias.unam.mx>
2022-04-21 17:28:09 -0500

    Inicio del fragment de registro

--- app/src/main/java/com/example/demopeliculasnavdrawer/ui/peliculas/PeliculasFragment.kt
+++ app/src/main/java/com/example/demopeliculasnavdrawer/ui/peliculas/PeliculasFragment.kt
@@ -1,4 +1,4 @@
-package com.example.demopeliculasnavdrawer.ui.slideshow
+package com.example.demopeliculasnavdrawer.ui.peliculas
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.demopeliculasnavdrawer.databinding.FragmentSlideshowBinding
+import com.example.demopeliculasnavdrawer.databinding.FragmentPeliculasBinding
 
-class SlideshowFragment : Fragment() {
+class PeliculasFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentPeliculasBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val galleryViewModel =
+            ViewModelProvider(this).get(PeliculasViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentPeliculasBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textGallery
+        galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/demopeliculasnavdrawer/ui/registro/RegistroViewModel.kt
+++ app/src/main/java/com/example/demopeliculasnavdrawer/ui/registro/RegistroViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.demopeliculasnavdrawer.ui.slideshow
+package com.example.demopeliculasnavdrawer.ui.registro
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class RegistroViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Ingrese sus datos para el registro"
     }
     val text: LiveData<String> = _text
 }
