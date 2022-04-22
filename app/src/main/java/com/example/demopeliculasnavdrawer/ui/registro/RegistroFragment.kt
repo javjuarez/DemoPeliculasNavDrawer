29d95e0266955e401e3d4572e793813b741f91a8
javjuarez <sonico@ciencias.unam.mx>
2022-04-22 02:02:40 -0500

    Fragment de registro completado

--- app/src/main/java/com/example/demopeliculasnavdrawer/ui/registro/RegistroFragment.kt
+++ app/src/main/java/com/example/demopeliculasnavdrawer/ui/registro/RegistroFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.demopeliculasnavdrawer.databinding.FragmentRegistroBinding
@@ -12,6 +14,8 @@ import com.example.demopeliculasnavdrawer.databinding.FragmentRegistroBinding
 class RegistroFragment : Fragment() {
 
     private var _binding: FragmentRegistroBinding? = null
+    private val regexPassword = Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$")
+    private val regexEmail = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,11 +36,62 @@ class RegistroFragment : Fragment() {
         slideshowViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        with(binding) {
+            textEditNombre.addTextChangedListener {
+                textLayoutNombre.isErrorEnabled = false
+                textLayoutNombre.error = null
+            }
+            textEditCorreo.addTextChangedListener {
+                textLayoutCorreo.isErrorEnabled = false
+                textLayoutCorreo.error = null
+            }
+            textEditPassword.addTextChangedListener {
+                textLayoutPassword.isErrorEnabled = false
+                textLayoutPassword.error = null
+            }
+            buttonRegistro.setOnClickListener {
+                checkTextFields(this)
+            }
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun checkTextFields(binding: FragmentRegistroBinding) {
+        var camposValidos = true
+        with(binding) {
+            // Nombre
+            val nombreText = textEditNombre.text.toString()
+            if (nombreText == "") {
+                textLayoutNombre.error = "El nombre no puede estar vacío."
+                camposValidos = false
+            }
+            // Email
+            val emailText = textEditCorreo.text.toString()
+            if (emailText == "") {
+                textLayoutCorreo.error = "El correo no puede estar vacío."
+                camposValidos = false
+            }
+            if (!regexEmail.matches(emailText)) {
+                textLayoutCorreo.error = "Ingrese un correo válido"
+                camposValidos = false
+            }
+            // Password
+            val passwordText = textEditPassword.text.toString()
+            if (!regexPassword.matches(passwordText)) {
+                textLayoutPassword.error =
+                    "La contraseña debe tener al menos 6 caracteres, una letra y un número"
+                camposValidos = false
+            }
+        }
+        if (camposValidos) {
+            Toast.makeText(context, "Todos los campos son válidos", Toast.LENGTH_SHORT).show()
+        }
     }
 }
